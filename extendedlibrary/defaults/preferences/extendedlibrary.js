595723de8b8315d6fd1e755faf50c27142902a09
Lukas Diener <lukas.diener@hotmail.com>
2010-05-20 18:45:29 +0000

    Added basic pref window. Fixes #6.

--- extendedlibrary/defaults/preferences/extendedlibrary.js
+++ extendedlibrary/defaults/preferences/extendedlibrary.js
@@ -1,2 +1,5 @@
+pref("extensions.extendedlibrary.boolpref", false);
+pref("extensions.extendedlibrary.intpref", 0);
+pref("extensions.extendedlibrary.stringpref", "A string");
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.extendedlibrary@lukasskywalker.description", "chrome://extendedlibrary/locale/extendedlibrary.properties");
